1ade8c023582eda3ce638add37b01e6177a167c1
2jiyong <jdragon2000@naver.com>
2024-12-17 14:32:29 +0900

    makeArr 함수를 테두리를 만들도록 변경

--- lotto.py
+++ lotto.py
@@ -2,7 +2,6 @@ def main():
     arr=makeArr()
     printArr(arr)
 
-
 def printArr(arr):
     for elems in arr:
         for elem in elems:
@@ -10,7 +9,15 @@ def printArr(arr):
         print()
         
 def makeArr():
-    arr=[['@']*10 for _ in range(10)]
+    arr=[[' ']*12 for _ in range(12)]
+    for i in range(12):
+        for j in range(12):
+            if i==0 or i==11:
+                arr[i][j]='-'
+            if j==0 or j==11:
+                arr[i][j]='|'
+            if (i==0 or i==11) and (j==0 or j==11):
+                arr[i][j]='+' 
     return arr
 
 if __name__ == '__main__':
